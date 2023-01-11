319a50c4f08aa90c453230a6b902086edd2375ef
nHellstrom <nikhellstrom@hotmail.com>
2023-01-11 16:13:18 +0100

    style

--- src/Components/PuppyIndividual.tsx
+++ src/Components/PuppyIndividual.tsx
@@ -13,7 +13,7 @@ const PuppyIndividual = (props: IPuppy) => {
     // }, )
 
     return <div className="Puppy__Card">
-        <img className="Puppy__image" src="https://images.unsplash.com/photo-1546527868-ccb7ee7dfa6a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"></img>
+        <img className="Puppy__image" src="https://images.unsplash.com/photo-1546527868-ccb7ee7dfa6a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80"></img>
         <ul className="Puppy__info">
             <li><b>Name:</b><br/> {props.name ? props.name : "Missing"}</li>
             <li><b>Breed:</b><br/> {props.breed ? props.breed : "Missing"}</li>
